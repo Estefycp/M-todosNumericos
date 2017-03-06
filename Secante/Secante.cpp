bb972c194d1ab8369e09279d1a793a8097b05306
Estefy <estefy.cp@hotmail.com>
2017-03-05 22:40:58 -0600

    Update Secante.cpp

--- Secante/Secante.cpp
+++ Secante/Secante.cpp
@@ -1,40 +1,33 @@
 #include <iostream>
 #include <cmath>
+#define ERRORMIN 1E-12
 using namespace std;
 
 //Metodo de Secante A00344189 Estefy A00344175 Carlos Rueda
+
+double f (double x)
+{
+     return (x*x*x)-(3*x*x)-4*x+2;
+}
 int main ()
 {
-   double xprox, xahora, xantes, fxprox, error, fxn, fxant;
-   int itrr=1;
-   cout<<"Valor do error: ";
+   double temp, x1, x2,  error;
+    int i = 0;
+   cout<<"Valor de error: ";
    cin>>error;
-   cout<<"valor de (xn-1): ";
-   cin>>xantes;
-   cout<<"Valor de (xn): ";
-   cin>>xahora;
+   cout<<"valor de (x0): ";
+   cin>>x1;
+   cout<<"Valor de (x1): ";
+   cin>>x2;
    cout<<endl;
-   fxn = xahora*xahora + xahora -6;
-   fxant = xantes*xantes + xantes -6;
-   cout<<"It || xn-1  || xn   || xn+1   ||  fxn+1  ||  |xn-xn-1| ||"<<endl;
-   while (nit < 500)
-   {
-      fxn = xahora*xahora + xahora -6;
-      fxant = xantes*xantes + xantes -6;
-      xprox = xahora - (((xahora - xantes)*fxn)/(fxn - fxant));
-      fxprox = xprox*xprox + xprox -6;
-      cout<<nit<<"  ||"<<xantes<<"  ||"<<xahora<<"  ||"<<xprox<<" ||"<<fxprox<<"  || "<<abs(xprox - xahora)<<" ||"<<endl;
-  if (abs(xprox-xahora)<error)
-  {
-      itrr = 500;
-  }
-  if (fxprox = 0)
-  {
-     itrr = 500;
-  }
-    xantes=xahora;
-    xahora=xprox;
-    itrr++;
- }
-  cout<<endl<<"La razi mas proxima es :  "<<xprox<<endl;
+
+
+   do{
+      
+      temp = x1;
+      x1= x2;
+      x2 = x1 - (x1-temp) * f(x1) / (f(x1) - f(temp));
+      i++;   
+   } while ( fabs (x1-x2) > error );
+  cout<<endl<<"La raiz mas proxima es :  "<<x2<< " con " << i << " iteraciones" endl;
 }
