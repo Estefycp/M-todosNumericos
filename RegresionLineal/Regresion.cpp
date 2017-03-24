6623e2a9fa3328c0c2dd96753d1393b26d60fe84
Estefy <estefy.cp@hotmail.com>
2017-03-24 13:27:38 -0600

    Update Regresion.cpp

--- RegresionLineal/Regresion.cpp
+++ RegresionLineal/Regresion.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 int main()
 {
-
     double x[] = {10.2,11.4,11.5,12.5,13.1,13.4,13.6,15,15.2,15.3,15.6,16.4,16.5,17,17.1};
     double y[] = {69.81,82.75,81.75,80.38,85.89,75.32,69.81,78.54,81.29,99.2,86.35,110.23,106.55,85.50,90.02};
     int i,j,k,n;
@@ -28,11 +27,8 @@ int main()
 
     }
 
-
     a0=(n*xysum-xsum*ysum)/(n*x2sum-xsum*xsum); // pendiente
     a1=(x2sum*ysum-xsum*xysum)/(x2sum*n-xsum*xsum); // interseccion
-
-
 
     cout<<"La regresion linear es y= "<<a0<<"x + "<<a1<<endl;
 
@@ -61,16 +57,9 @@ int main()
         }
     cout<<"\n Coeficiente de la relacion: "<<pxy/sqrt(sx2*sy2)<<"\n";
 
-
-
-
     EE=sqrt(err/(n - 2));
     cout<<"\n Error estandar "<<  EE;
 
-
-
     return 0;
-
-
 
 }
